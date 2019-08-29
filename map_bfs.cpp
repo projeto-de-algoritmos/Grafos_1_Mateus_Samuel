19004f99e9aeeb994ad99052edf59ad4010b5ac2
Mateus Nóbrega <mateusn@outlook.com>
2019-08-29 19:08:13 -0300

    Adding comments and a sample

--- map_bfs.cpp
+++ map_bfs.cpp
@@ -2,31 +2,43 @@
 #include <vector>
 #include <utility>
 #include <queue>
+#include <cstdio>
 
-using namespace std;
-using vi=vector<int>;
-using ull=unsigned long long;
+#define X_MAX 1005
+#define Y_MAX 1005
 
-bool vs[1005][1005];
+bool visited[X_MAX][Y_MAX];
 
-bool safe(int i, int j, int n, int m)
+/*
+ * Returns true if a given coordinate is valid (it doesn't exceed the matrix
+ * size)
+ */
+bool is_valid(int i, int j, int n, int m)
 {
-	if (i < 0 || i >= n) return false;
-	if (j < 0 || j >= m) return false;
+	if (i < 0 or i >= n) return false;
+	if (j < 0 or j >= m) return false;
 
 	return true;
 }
 
-int solve(const vector<vector<int>> &tab, pair<int, int> & start, pair<int, int> & end, int n, int m)
+int solve(
+		const std::vector<std::vector<int>> &tab,
+		const std::pair<int, int> & start, 
+		const std::pair<int, int> & end, 
+		int n, int m)
 {
-	queue<pair<pair<int, int>, int>> q;
+	/*  pair of a coordinate and the number of moves until that point
+	 *  example: {{1, 2}, 4} means that 4 steps were necessary to reach the
+	 *  coordinate [1,2]
+	 */
+	std::queue<std::pair<std::pair<int, int>, int>> q; 
 
-	q.push({{start.first, start.second}, 1});
-	vs[start.first][start.second] = 1;
+	q.push({{start.first, start.second}, 0});
+	visited[start.first][start.second] = true;
 
 	while (not q.empty())
 	{
-		pair<int, int> u = q.front().first;
+		std::pair<int, int> u = q.front().first;
 		int nops = q.front().second;
 		q.pop();
 
@@ -34,22 +46,25 @@ int solve(const vector<vector<int>> &tab, pair<int, int> & start, pair<int, int>
 			return nops;
 
 
-		vector<pair<int, int>> adj;
+		std::vector<std::pair<int, int>> adj;
 
-		if (safe(u.first, u.second + 1, n, m) && tab[u.first][u.second + 1] > 0)
+		if (is_valid(u.first, u.second + 1, n, m) and tab[u.first][u.second + 1] > 0)
 			adj.push_back({u.first, u.second + 1});
-		if (safe(u.first, u.second - 1, n, m) && tab[u.first][u.second - 1] > 0)
+
+		if (is_valid(u.first, u.second - 1, n, m) and tab[u.first][u.second - 1] > 0)
 			adj.push_back({u.first, u.second - 1});
-		if (safe(u.first + 1, u.second, n, m) && tab[u.first + 1][u.second] > 0)
+
+		if (is_valid(u.first + 1, u.second, n, m) and tab[u.first + 1][u.second] > 0)
 			adj.push_back({u.first + 1, u.second});
-		if (safe(u.first - 1, u.second, n, m) && tab[u.first - 1][u.second] > 0)
+
+		if (is_valid(u.first - 1, u.second, n, m) and tab[u.first - 1][u.second] > 0)
 			adj.push_back({u.first - 1, u.second});
 
 		for (auto p : adj)
 		{
-			if (!vs[p.first][p.second])
+			if (not visited[p.first][p.second])
 			{
-				vs[p.first][p.second] = 1;
+				visited[p.first][p.second] = 1;
 				q.push({p, nops+1});
 			}
 		}
@@ -59,30 +74,34 @@ int solve(const vector<vector<int>> &tab, pair<int, int> & start, pair<int, int>
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+	std::freopen("map.txt", "r", stdin);
 
 	int n, m; 
-	cin >> n >> m;
-	pair<int, int> start, end;
-	vector<vector<int>> tab(n, vector<int>(m));
+	std::cin >> n >> m;
 
+	std::pair<int, int> start, end;
+	std::vector<std::vector<int>> tab(n, std::vector<int>(m));
+	
+	//TODO: Use char instead of int
+	
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 		{
-			cin >> tab[i][j];
+			std::cin >> tab[i][j];
 
 			if (tab[i][j] == 2)
-				start = make_pair(i, j);
+				start = std::make_pair(i, j);
 			
 			if (tab[i][j] == 3)
-				end = make_pair(i, j);
+				end = std::make_pair(i, j);
 		}
 
-	int ans = solve(tab, start, end, n, m);
+	int no_of_steps = solve(tab, start, end, n, m);
 
-	cout << ans << "\n";
-	
+	if (no_of_steps != -1)
+		std::cout << "Menor quantidade de movimentos necessarios para chegar ao fim: " << no_of_steps << "\n";
+	else
+		std::cout << "Nao ha caminho valido\n";
     
     return 0;
 }
