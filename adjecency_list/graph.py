f818f0b2b6062def34c80cc43d18942382591fe5
Samuel Borges <samuelmordred@gmail.com>
2019-09-02 23:05:46 -0300

    added print graph funcionality

--- adjecency_list/graph.py
+++ adjecency_list/graph.py
@@ -22,26 +22,26 @@ def linked_list(array):
     for lines in array:
         for digit in lines:
             if(digit != "#"):
-                head_node = Node(i,j)
+                head_node = Node(array, i,j)
                 aux_node = head_node
 
                 if(is_valid(array,i-1,j)):
-                    new_node = Node(i-1,j)
+                    new_node = Node(array, i-1, j)
                     aux_node.set_next(new_node)
                     aux_node = new_node
 
                 if(is_valid(array,i,j-1)):
-                    new_node = Node(i,j-1)
+                    new_node = Node(array, i, j-1)
                     aux_node.set_next(new_node)
                     aux_node = new_node
 
                 if(is_valid(array,i,j+1)):
-                    new_node = Node(i,j+1)
+                    new_node = Node(array, i, j+1)
                     aux_node.set_next(new_node)
                     aux_node = new_node
 
                 if(is_valid(array,i+1,j)):
-                    new_node = Node(i+1,j)
+                    new_node = Node(array, i+1, j)
                     aux_node.set_next(new_node)
                 
                 adjacency_list.append(head_node)
@@ -53,12 +53,32 @@ def linked_list(array):
 
     return adjacency_list
 
-     
+
+def print_graph(adjacency_list):
+    for node in adjacency_list:
+        aux = node
+        while aux is not None:
+            print(aux.get_value(),"|",  aux.get_data(), "-->", end='')
+            aux = aux.get_next()
+        print("None", end="")
+        print("\n")
+
+
+    
+                
+                
+
+                
+                
+
+
+
 
         
 
-            
-            
+
+
+
 
 
 
